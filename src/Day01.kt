252617a81d70e4a2e17e49544a1fbd072693baab
Matej Focko <mfocko@redhat.com>
2021-12-04 13:26:57 +0100

    day(01): add solution
    Signed-off-by: Matej Focko <mfocko@redhat.com>

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,18 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        return input.windowed(2).count { it[0] < it[1] }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        return part1(input.windowed(3).map { it.sum() }.toList())
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInputAsInts("Day01_test")
+    val input = readInputAsInts("Day01")
 
-    val input = readInput("Day01")
+    check(part1(testInput) == 7)
     println(part1(input))
+
+    check(part2(testInput) == 5)
     println(part2(input))
 }
